8f55d343583d8a7f63d5d592207f52e0d29f4da7
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2023-09-15 15:12:40 +0600

    addeded course list style edited and overall functionality checked

--- src/Components/CourseCart/CourseCart.jsx
+++ src/Components/CourseCart/CourseCart.jsx
@@ -6,10 +6,12 @@ const CourseCart = ({ selectedCourse, totalCredit, remainig }) => {
         <div className="lg:w-1/4 mt-10 lg:mt-0">
             <h2 className="text-2xl border-b-2 mb-5 font-semibold text-[#2F80ED]">Credit Hour Reamaining {remainig} hr</h2>
             <h3 className="text-2xl font-semibold mb-5">Course Name:</h3>
-            {
+           <ol className="list-decimal">
+           {
                 selectedCourse.map((select, idx) => (<SelectedCourse
                     key={idx} select={select} ></SelectedCourse> ))
             }
+           </ol>
             <h2 className="text-2xl border-t-2 mt-5 font-semibold">Total Credit Hour:{totalCredit}</h2>
         </div>
     );
